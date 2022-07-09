bb2dc1761d174cd07e0832d24cad923a9da2055d
Matthew-mkII <matsubara.hiroaki.etrobo@outlook.com>
2022-07-09 23:28:54 +0900

    論理演算子の本当の意味を知ろう。

--- src/index.js
+++ src/index.js
@@ -201,3 +201,23 @@ const nameArr = ["Alice", "Bob", "Carol"];
 // };
 // console.log(checkSum(900, 122));
 // console.log(checkSum(10, 40));
+
+/**
+ * 論理演算子の本当の意味を知ろう
+ */
+// const flag1 = true;
+// const flag2 = false;
+
+// // if (flag1 || flag2) {
+// //   console.log("1か2はtrueになります。");
+// //
+
+// // || は左側がfalseなら右側を返す。左側がtrueなら左側を返す
+// const num = null;
+// const fee = num  || "金額未設定です";
+// console.log(fee);
+
+// //&& は左側がtrueなら右側を返す。
+// const num2 = 100;
+// const fee2 = num2 && "何か設定されました";
+// console.log(fee2)
