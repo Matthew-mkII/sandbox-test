1d1a562571e758bdad57b605627dba15436fecdd
Matthew-mkII <matsubara.hiroaki.etrobo@outlook.com>
2022-07-09 22:23:34 +0900

    const、letの使い方を学んだ。

--- src/index.js
+++ src/index.js
@@ -44,7 +44,47 @@
 // console.log(val4);
 
 //constで定義した配列はオブジェクトの変更が可能
-const val5 = ["dog", "cat"];
-val5[0] = "bird";
-val5.push("monkey");
-console.log(val5);
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+
+//  const name = "matthew";
+//  const age = 37;
+
+//  //私の名前はmatthewです。年齢は37歳です。
+
+//  //従来の方法
+//  const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+//  console.log(message1);
+
+//  ///テンプレート文字列を用いた方法
+//  const message2 = `私の名前は${name}です。年齢は${age}です。`;
+//  console.log(message2);
+
+/**
+ * アロー関数
+ */
+//従来の関数
+// function func1(str) {
+//   return str;
+// };
+const func1 = function (str) {
+  return str;
+};
+console.log(func1("func1です"));
+
+//アロー関数
+const func2 = (str) => str;
+
+console.log(func2("func2です"));
+
+const func3 = (num1, num2) => {
+  return num1 + num2;
+};
+
+console.log(func3(10, 30));
